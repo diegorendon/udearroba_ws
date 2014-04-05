f6cac508c6e11ffdefcef9847377feb5cdeaea91
Diego Rendón <diego3d3@gmail.com>
2014-04-05 04:19:54 -0500

    Creación de un DTO para la información de autenticación.

---
+++ src/java/co/edu/udea/udearroba/dto/AuthenticationInformation.java
@@ -0,0 +1,37 @@
+package co.edu.udea.udearroba.dto;
+
+/**
+ * The Data Transfer Object for the authentication information entities.
+ * 
+ * @author Diego
+ */
+public class AuthenticationInformation {
+    private String username;
+    private String password;
+    private String identification;
+
+    public String getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+}
