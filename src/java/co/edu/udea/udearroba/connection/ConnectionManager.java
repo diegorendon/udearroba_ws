092a81e08961dda1280983dda83009884fe0cc05
Diego Rendón <diego3d3@gmail.com>
2014-04-07 13:22:41 -0500

    Refactorización y adición de comentarios y documentación faltante en ConnectionManager.

--- src/java/co/edu/udea/udearroba/connection/ConnectionManager.java
+++ src/java/co/edu/udea/udearroba/connection/ConnectionManager.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package co.edu.udea.udearroba.connection;
 
 import java.sql.Connection;
@@ -13,36 +7,56 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 /**
+ * Manage the connection to the databases.
  *
  * @author Diego
  */
 public class ConnectionManager {
-    public static final int MARES = 1;
-    public static final int SERVA = 2;
 
+    /**
+     * Constant index for the SERVA database connection.
+     */
+    public static final int SERVA = 1;
+//    public static final int SIPE = 2;
+//    public static final int MARES = 3;
+
+    /**
+     * Creates and return the connection to a database.
+     *
+     * @param database the integer index that indicates to which database will
+     * establish the connection.
+     *
+     * @return a Connection object to the selected database.
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     * @throws Exception
+     */
     public static Connection getConnection(int database) throws ClassNotFoundException, SQLException, Exception {
         try {
             String bundle;
             switch (database) {
-                case MARES:
-                    bundle = "co.edu.udea.udearroba.properties.DBMares";
-                    break;
                 case SERVA:
-                    bundle = "co.edu.udea.udearroba.properties.DBServa";
+                    bundle = "co.edu.udea.udearroba.properties.SERVADataBase";
                     break;
+//                case SIPE:
+//                    bundle = "co.edu.udea.udearroba.properties.SIPEDataBase";
+//                    break;
+//                case MARES:
+//                    bundle = "co.edu.udea.udearroba.properties.MARESDataBase";
+//                    break;
                 default:
                     bundle = "";
             }
             ResourceBundle rb = ResourceBundle.getBundle(bundle);
-            Class.forName(rb.getString("driverDB"));
-            Connection connection = DriverManager.getConnection(rb.getString("urlDB"), rb.getString("user"), rb.getString("passDB"));
+            Class.forName(rb.getString("dataBaseDriver"));
+            Connection connection = DriverManager.getConnection(rb.getString("dataBaseURL"), rb.getString("dataBaseUser"), rb.getString("dataBasePassword"));
             return connection;
         } catch (ClassNotFoundException ex) {
-            throw new ClassNotFoundException("No se encuentra el driver de conexión.");
+            throw new ClassNotFoundException("No se encuentra el driver de conexión."); // FIXME: move strings to a buundle;
         } catch (SQLException ex) {
-            throw new SQLException("Ha ocurrido un error de SQL.");
+            throw new SQLException("Ha ocurrido un error de SQL.");     // FIXME: move strings to a buundle;
         } catch (MissingResourceException ex) {
-            throw new Exception("No se encuentra el recurso de propiedades para las consultas.");
+            throw new Exception("No se encuentra el recurso de propiedades para las consultas.");   // FIXME: move strings to a buundle;
         }
     }
 }
