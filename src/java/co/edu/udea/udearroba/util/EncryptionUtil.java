ed80468820201ad680fe748ed56d296c27e9f91c
Diego Rendón <diego3d3@gmail.com>
2014-04-26 13:12:17 -0500

    Eliminación de la ecriptación md5 de la clave secreta para el cifrado AES.

--- src/java/co/edu/udea/udearroba/util/EncryptionUtil.java
+++ src/java/co/edu/udea/udearroba/util/EncryptionUtil.java
@@ -2,9 +2,6 @@ package co.edu.udea.udearroba.util;
 
 import co.edu.udea.udearroba.bl.services.AuthenticationManager;
 import co.edu.udea.udearroba.i18n.Texts;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.crypto.Cipher;
@@ -31,7 +28,7 @@ public class EncryptionUtil {
     public static String decrypt(String password, String initializationVectorString, String secretKey) {
         String decryptedPassword = null;
         try {
-            SecretKeySpec secretkeySpec = new SecretKeySpec(md5(secretKey).getBytes(), "AES");
+            SecretKeySpec secretkeySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
             IvParameterSpec initialVector = new IvParameterSpec(initializationVectorString.getBytes());
             Cipher cipher = Cipher.getInstance("AES/CFB8/NoPadding");
             cipher.init(Cipher.DECRYPT_MODE, secretkeySpec, initialVector);
@@ -42,19 +39,5 @@ public class EncryptionUtil {
             Logger.getLogger(AuthenticationManager.class.getName()).log(Level.SEVERE, Texts.getText("decryptionLogMessage"), ex);
         }
         return decryptedPassword;
-    }
-    
-    /**
-     * Generates the md5 hash of the input string.
-     *
-     * @param input The data string in plain text.
-     *
-     * @return String The md5 hash of the input string.
-     */
-    private static String md5(String input) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        byte[] messageDigest = md.digest(input.getBytes());
-        BigInteger number = new BigInteger(1, messageDigest);
-        return number.toString(16);
     }
 }
