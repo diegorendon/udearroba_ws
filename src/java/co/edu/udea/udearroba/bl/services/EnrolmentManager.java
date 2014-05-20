eec8eb581916ac87e577e7956939b5fcf5360c8e
Diego Rendón <diego3d3@gmail.com>
2014-05-20 13:54:17 -0500

    Creación de los recursos /useracademicinformation y /courses que se exponen como servicios Web REST.
    /useracademicinformation para obtener la información académica de todos los semestres del estudiante y /courses para obtener la lista de cursos del último semestre del estudiante.

---
+++ src/java/co/edu/udea/udearroba/bl/services/EnrolmentManager.java
@@ -0,0 +1,121 @@
+package co.edu.udea.udearroba.bl.services;
+
+import co.edu.udea.exception.OrgSistemasSecurityException;
+import co.edu.udea.udearroba.dto.AcademicInformation;
+import co.edu.udea.udearroba.dto.Course;
+import co.edu.udea.udearroba.i18n.Texts;
+import co.edu.udea.wsClient.OrgSistemasWebServiceClient;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Manage the enrolment process of the UdeA Portal's users.
+ *
+ * @author Diego Rendón
+ */
+public class EnrolmentManager {
+
+    private boolean useTestingData;                                             // Switches between testing and production modes for the Web Services' calls. Set to true for development and false for production.
+    private String TOKEN;                                                       // Token assigned to Ude@ in order to be able to use the REST Web services.
+    private String PUBLIC_KEY;                                                  // Public key used by Ude@ in order to be able to use the REST Web services.
+    private String SECRET_KEY;                                                  // The 128-bits secret key used by Ude@ to encrypt/decrypt the data.
+    private String INITIALIZATION_VECTOR;                                       // The 16 bytes initialization vector used by Ude@ to encrypt/decrypt the data.
+    private final String MARES_ACADEMIC_INFO_REST_CALL = "consultainformacionacademicamares"; // The Web service to call to get the academic information of an user in MARES.
+    private final String MARES_ACADEMIC_INFO_PARAM1 = "cedula";                 // The Web service's param name used to get the user academic information.
+    private final String MARES_COURSES_REST_CALL = "consultamateriasestudiantemares"; // The Web service to call to get the courses list of an user in MARES.
+    private final String MARES_COURSES_PARAM1 = "cedula";                       // The Web service's first param name used to get the user's courses.
+    private final String MARES_COURSES_PARAM2 = "programa";                     // The Web service's second param name used to get the user's courses.
+    private final String MARES_COURSES_PARAM3 = "semestre";                     // The Web service's third param name used to get the user's courses.
+    private final String RESOURCE_BUNDLE_PATH = "co.edu.udea.udearroba.properties.Application"; // Resource bundle with the application properties
+    private ResourceBundle resource;
+    private Validator validator;
+
+    /**
+     * Default constructor without parameters.
+     */
+    public EnrolmentManager() {
+        try {
+            this.resource = ResourceBundle.getBundle(RESOURCE_BUNDLE_PATH, new Locale("es"));
+            this.useTestingData = Boolean.parseBoolean(resource.getString("useTestingData"));
+            this.TOKEN = resource.getString("token");
+            this.PUBLIC_KEY = resource.getString("publicKey");
+            this.SECRET_KEY = resource.getString("secretKey");
+            this.INITIALIZATION_VECTOR = resource.getString("initializationVector");
+            validator = new Validator();
+        } catch (MissingResourceException ex) {
+            Logger.getLogger(EnrolmentManager.class.getName()).log(Level.SEVERE, Texts.getText("applicationPropertiesLogMessage"), ex);
+        }
+    }
+
+    /**
+     * Returns the user's academic information from UdeA Portal's databases.
+     *
+     * @param identification The user identification.
+     *
+     * @return AcademicInformation An AcademicInformation DTO with the user's
+     * academic information or NULL.
+     */
+    public List<AcademicInformation> getAcademicInformation(String identification) {
+        OrgSistemasWebServiceClient RESTWebServiceClient = null;
+        try {
+            RESTWebServiceClient = new OrgSistemasWebServiceClient(useTestingData);
+        } catch (OrgSistemasSecurityException ex) {
+            Logger.getLogger(EnrolmentManager.class.getName()).log(Level.SEVERE, Texts.getText("RESTWebServiceClientLogMessage"), ex);
+        }
+        boolean isValidIdentification;
+        isValidIdentification = this.validator.validateIdentification(identification);
+        List<AcademicInformation> academicInformationList = null;
+        if (isValidIdentification && RESTWebServiceClient != null) {
+            RESTWebServiceClient.addParam(MARES_ACADEMIC_INFO_PARAM1, identification.trim());
+            academicInformationList = new ArrayList<AcademicInformation>();
+            try {
+                academicInformationList = RESTWebServiceClient.obtenerBean(MARES_ACADEMIC_INFO_REST_CALL, TOKEN, AcademicInformation.class);
+            } catch (OrgSistemasSecurityException ex) {
+                Logger.getLogger(EnrolmentManager.class.getName()).log(Level.SEVERE, Texts.getText("academicinformationRESTCallLogMessage"), ex);
+            }        
+        }
+        return academicInformationList;
+    }
+
+    /**
+     * Returns the user's courses list from UdeA Portal's databases.
+     *
+     * @param identification The user identification.
+     *
+     * @return List<Course> A list of Course DTOs with the user's information or NULL.
+     */
+    public List<Course> getCourses(String identification) {
+        OrgSistemasWebServiceClient RESTWebServiceClient = null;
+        try {
+            RESTWebServiceClient = new OrgSistemasWebServiceClient(useTestingData);
+        } catch (OrgSistemasSecurityException ex) {
+            Logger.getLogger(EnrolmentManager.class.getName()).log(Level.SEVERE, Texts.getText("RESTWebServiceClientLogMessage"), ex);
+        }
+        List<AcademicInformation> academicInformationList = this.getAcademicInformation(identification);
+        boolean isValidIdentification;
+        isValidIdentification = this.validator.validateIdentification(identification);
+        AcademicInformation currentSemesterInformation = null;
+        int lastSemesterIndex = 0; // The academic information of the last semester is retrieved at index 0 of the list. // TODO: validate if this is always the case
+        if (!academicInformationList.isEmpty() && academicInformationList.get(lastSemesterIndex) != null){
+            currentSemesterInformation = academicInformationList.get(lastSemesterIndex);
+        }
+        List<Course> courseList = null;
+        if (isValidIdentification && RESTWebServiceClient != null && currentSemesterInformation != null) {
+            RESTWebServiceClient.addParam(MARES_COURSES_PARAM1, identification);
+            RESTWebServiceClient.addParam(MARES_COURSES_PARAM2, currentSemesterInformation.getPrograma());
+            RESTWebServiceClient.addParam(MARES_COURSES_PARAM3, currentSemesterInformation.getSemestre());
+            courseList = new ArrayList<Course>();
+            try {
+                courseList = RESTWebServiceClient.obtenerBean(MARES_COURSES_REST_CALL, TOKEN, Course.class);
+            } catch (OrgSistemasSecurityException ex) {
+                Logger.getLogger(EnrolmentManager.class.getName()).log(Level.SEVERE, Texts.getText("coursesRESTCallLogMessage"), ex);
+            }            
+        }
+        return courseList;
+    }
+}
---
+++ src/java/co/edu/udea/udearroba/ws/resources/UserCourses.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.udea.udearroba.ws.resources;
+
+import co.edu.udea.udearroba.bl.services.EnrolmentManager;
+import co.edu.udea.udearroba.dto.Course;
+import co.edu.udea.udearroba.i18n.Texts;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+/**
+ * REST Web Service for user's courses information.
+ *
+ * @author Diego Rendón
+ */
+@Path("/courses")
+public class UserCourses {
+    
+    /**
+     * Web service for retrieving user's courses list from UdeA Portal's databases
+     * given the identification.
+     *
+     * @param identification The user identification.
+     *
+     * @return String The JSON data string with the user's courses list.
+     */
+    @GET
+    @Path("{identification}")
+    @Produces("application/json" + ";charset=utf-8")
+    public String getCourses(@PathParam("identification") String identification) {
+        EnrolmentManager enrolManager = new EnrolmentManager();
+        List<Course> courseList = enrolManager.getCourses(identification);
+        JSONObject JSONResponse = new JSONObject();
+        try {
+            List<Map<String, String>> courses = new ArrayList<Map<String, String>>();
+            if (courseList != null) {
+                for (int i = 0; i < courseList.size(); i++) {
+                    Map<String, String> courseInfoMap = new HashMap<String, String>();
+                    courseInfoMap.put("semestre", courseList.get(i).getSemestre());
+                    courseInfoMap.put("materia", courseList.get(i).getMateria());
+                    courseInfoMap.put("codigo", courseList.get(i).getCodigo());
+                    courseInfoMap.put("nombre", courseList.get(i).getNombre());
+                    courseInfoMap.put("grupo", courseList.get(i).getGrupo());
+                    courseInfoMap.put("creditos", courseList.get(i).getCreditos());
+                    courseInfoMap.put("tipoHomologacion", courseList.get(i).getTipoHomologacion());
+                    courseInfoMap.put("nota", courseList.get(i).getNota());
+                    courses.add(courseInfoMap);
+                }
+                JSONResponse.put("response", courses);
+            } else {
+                courses.clear();
+                JSONResponse.put("response", courses);
+            }
+        } catch (JSONException ex) {
+            Logger.getLogger(UserInformation.class.getName()).log(Level.SEVERE, Texts.getText("JSONResponseUserInfoLogMessage"), ex);
+        }
+        return JSONResponse.toString();
+    }
+}
