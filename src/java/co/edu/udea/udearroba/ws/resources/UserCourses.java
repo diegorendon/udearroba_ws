167a17b658de6461e92456442668987a77c1a6fb
Diego Rendón <diego3d3@gmail.com>
2014-09-26 11:03:18 -0500

    Eliminación de comentarios innecesarios.

--- src/java/co/edu/udea/udearroba/ws/resources/UserCourses.java
+++ src/java/co/edu/udea/udearroba/ws/resources/UserCourses.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package co.edu.udea.udearroba.ws.resources;
 
 import co.edu.udea.udearroba.bl.services.EnrolmentManager;
